a55bc4337e80c60f979561a963c6be1e38356d56
barathwaj0214 <145427130+barathwaj0214@users.noreply.github.com>
2023-09-26 12:56:38 +0530

    Add files via upload

---
+++ Module 2/gettingStartedWithjs.js
@@ -0,0 +1,27 @@
+// USUAL PRINT
+console.log("Getting Started With JS");
+console.warn("Hello,How are you");
+console.error("ERRRRORRR");
+
+// 1.) programming
+// A.) VARIABLES
+// declaration
+// initialization
+var age;// declaration
+var bag = "Books";
+var fullName = "BarathWaj";
+bag = "Tiffen Box";
+var count = 20 + 20;
+var englishmarks = 89;
+var tamilmarks = 92;
+console.log(englishmarks + tamilmarks);
+console.log(count);
+console.log(bag);
+age = 24;
+console.log(age);
+console.log(fullName);
+
+// 2.) Handling your views
+// ---> for controlling your html
+//---> controlling your forms,para,heading,buttons
+//---> DOM => Document Object Modeling
